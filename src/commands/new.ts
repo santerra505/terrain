5dca0d4df1aaafc112d912384b668f11c89391d6
Artem <rtkushner@gmail.com>
2022-04-05 08:39:00 -0700

    moved from cargo generate to plain download with edits

--- src/commands/new.ts
+++ src/commands/new.ts
@@ -23,45 +23,65 @@ export default class New extends Command {
   static args = [{ name: "name", required: true }];
 
   async run() {
+
     const { args, flags } = this.parse(New);
 
     cli.log("generating: ");
+    // ------------------------------- Scaffolding
     cli.action.start("- contract");
 
     if (flags.path) {
+
+      console.log(`[ new.ts ]: changed dir to ${flags.path} `)
       process.chdir(flags.path);
     }
 
+    console.log(`[ new.ts ]: created dir: ${args.name} `)
     fs.mkdirSync(args.name);
     process.chdir(args.name);
 
     fs.mkdirSync("contracts");
     process.chdir("contracts");
 
-    execSync(
-      `cargo generate --git https://github.com/CosmWasm/cw-template.git --branch ${flags.version} --name counter`
-    );
+    // ------------------------------- Contract template
+    // https://github.com/InterWasm/cw-template/archive/refs/heads/main.zip
 
-    cli.action.stop();
+    console.log("Generating cargo template via git download. rEVAMP")
+    // execSync(`cargo generate --git https://github.com/CosmWasm/cw-template.git --branch ${flags.version} --name counter`);
 
-    process.chdir("..");
 
-    cli.action.start("- frontend");
-    const file = fs.createWriteStream("frontend.zip");
 
+
+    const repoName     = "cw-template"
+    const branch       = "main";
+
+    const contractFile = fs.createWriteStream("contract.zip");
     await new Promise((resolve, reject) => {
       request
-        .get(
-          "https://github.com/terra-money/terrain-frontend-template/archive/refs/heads/main.zip"
-        )
-        .on("error", (error) => {
-          reject(error);
-        })
+        .get(`https://github.com/InterWasm/${repoName}/archive/refs/heads/${branch}.zip`)
+        .on("error", (error) => {reject(error);})
+        .pipe(contractFile)
+        .on("finish", () => {cli.action.stop();resolve(null);});
+    });
+
+    const contractZip = new Zip("contract.zip");
+    contractZip.extractAllTo(".", true);
+
+    fs.renameSync(`${repoName}-${branch}`, "counter");
+    fs.removeSync("contract.zip");
+
+    process.chdir("..");
+    cli.action.stop();
+
+    // ------------------------------- Frontend template
+    cli.action.start("- frontend");
+    const file = fs.createWriteStream("frontend.zip");
+    await new Promise((resolve, reject) => {
+      request
+        .get("https://github.com/terra-money/terrain-frontend-template/archive/refs/heads/main.zip")
+        .on("error", (error) => {reject(error);})
         .pipe(file)
-        .on("finish", () => {
-          cli.action.stop();
-          resolve(null);
-        });
+        .on("finish", () => {cli.action.stop();resolve(null);});
     });
 
     const zip = new Zip("frontend.zip");
